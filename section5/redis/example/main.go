5f4d171e74a6164610e74c1dd77cf8eef9482466
Bob Rusch <rvrusch@gmail.com>
2022-08-13 18:01:10 -0400

    initial simple redis complete

---
+++ section5/redis/example/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/bobrusch/go-solutions/section5/redis"
+
+func main() {
+	if err := redis.Exec(); err != nil {
+		panic(err)
+	}
+
+	if err := redis.Sort(); err != nil {
+		panic(err)
+	}
+}
---
+++ section5/redis/go.mod
@@ -0,0 +1,10 @@
+module github.com/bobrusch/go-solutions/section5/redis
+
+go 1.19
+
+require gopkg.in/redis.v5 v5.2.9
+
+require (
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+)
